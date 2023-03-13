637ee6e9e90d56fc5622bfebfc3262772b5b25a8
TheRickStick <114582372+TheRickStick@users.noreply.github.com>
2023-03-13 10:51:14 +0100

    first

---
+++ routes/tutorials.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+var TutorialService = require("../services/TutorialService")
+var db = require("../models");
+var tutorialService = new TutorialService(db);
+const { Op } = require("sequelize");
+
+const getPagination = (page, size) => {
+  const limit = size ? +size : 5;
+  const offset = page ? (page-1) * limit : 0;
+  return { limit, offset };
+};
+
+/* GET tutorials listing. */
+router.get('/', async function(req, res, next) {
+  const { sort, title, description, published, page, size} = req.query;
+  const order = sort ? sort.split(',').map(pair => pair.split(':')) : [];
+  const titleCondition = title ? { title: { [Op.like]: `%${title}%` } } : null;
+  const descCondition = description ? { description: { [Op.like]: `%${description}%` } } : null;
+  const publishedCondition = published ? { published: { [Op.like]: `%${published}%` } } : null;
+  const condition = {[Op.and]: [titleCondition, descCondition, publishedCondition]};
+  const pagination = getPagination(page, size);
+  const tutorials = await tutorialService.getAll(condition, order, pagination);
+  res.render('tutorials', {tutorials: tutorials});
+});
+module.exports = router;
